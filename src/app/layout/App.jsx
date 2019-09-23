d237e80895d542d4cf3d906d3e97bd70ee44e15a
elementarykid <wombattywombat@gmail.com>
2019-09-22 22:23:19 -0400

    e

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
+import {Button} from 'semantic-ui-react';
 //import './App.css';
 
 class App extends Component{
   render() {
     return (
-      <div>
+      <div className='app'>
         <h1>Re-vents</h1>
+        <button className="ui icon button">
+          <i className="smile icon">
+            CSS Button
+          </i>
+        </button>
+        <Button icon="smile" content="React Button" />
       </div>
     );
   }
