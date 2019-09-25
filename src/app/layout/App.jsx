303d64c58efa20c633c83604142b010ce1ae2d0b
elementarykid <wombattywombat@gmail.com>
2019-09-25 09:55:45 -0400

    o;uilykrdteswcbiypuoyltjrnmhezdfxcmh.ujipho87keb rntfygu.

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,19 +1,19 @@
-import React, {Component} from 'react';
-import {Button} from 'semantic-ui-react';
+import React, {Component,Fragment} from 'react';
+import EventDashboard from '../../features/event/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/navBar/NavBar';
+import { Container } from 'semantic-ui-react';
+//import {Button} from 'semantic-ui-react';
 //import './App.css';
 
 class App extends Component{
   render() {
     return (
-      <div className='app'>
-        <h1>Re-vents</h1>
-        <button className="ui icon button">
-          <i className="smile icon">
-            CSS Button
-          </i>
-        </button>
-        <Button icon="smile" content="React Button" />
-      </div>
+      <Fragment>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </Fragment>
     );
   }
 }
