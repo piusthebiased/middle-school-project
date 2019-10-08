93cef775d69ebf36b586ae0b47d4f5db7893805f
elementarykid <wombattywombat@gmail.com>
2019-10-07 20:20:13 -0400

    yay

--- src/features/event/eventDashboard/EventDashboard.jsx
+++ src/features/event/eventDashboard/EventDashboard.jsx
@@ -112,6 +112,7 @@ class EventDashboard extends Component {
           />
           {isOpen && (
             <EventForm
+            key={selectedEvent ? selectedEvent.id : 0}
               selectedEvent={selectedEvent}
               createEvent={this.handleCreateEvent}
               cancelFormOpen={this.handleFormCancel}
